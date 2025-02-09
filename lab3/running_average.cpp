c47faeaa378e411d6d59daf3a1fff0ee47273538
Matias Ruonala <placeholder@asdf.com>
2025-02-09 14:17:36 +0200

    lab4 beginnings

--- lab3/running_average.cpp
+++ lab3/running_average.cpp
@@ -3,53 +3,57 @@
 #include <string>
 
 class RunningAverage {
-public:
-  RunningAverage();
-  RunningAverage(const int &size);
-  void add_value(int value);
-  double get_average();
+  public:
+    RunningAverage();
+    RunningAverage(const int &size);
+    void add_value(int value);
+    double get_average();
 
-private:
-  std::deque<int> d;
-  uint8_t size;
+  private:
+    std::deque<int> d;
+    int size;
 };
 
 int main() {
-  RunningAverage avg;
-  std::string input;
-  int number = 1;
-  do {
-    std::cout << "Enter numbers, '0' to stop: ";
-    std::getline(std::cin, input);
-    try {
-      number = std::stoi(input);
-      if (number != 0) {
-        avg.add_value(number);
-      }
-    } catch (const std::invalid_argument &e) {
-      std::cerr << "Bad input. Enter valid number" << std::endl;
-    } catch (const std::out_of_range &e) {
-      std::cerr << "Number is out of range. Use another number" << std::endl;
-    }
-    std::cout << avg.get_average() << std::endl;
-  } while (number != 0);
+    RunningAverage avg;
+    std::string input;
+    int number = 1;
+    do {
+        std::cout << "Enter numbers, '0' to stop: ";
+        std::getline(std::cin, input);
+        try {
+            number = std::stoi(input);
+            if (number != 0) {
+                avg.add_value(number);
+            }
+        } catch (const std::invalid_argument &e) {
+            std::cerr << "Bad input. Enter valid number" << std::endl;
+        } catch (const std::out_of_range &e) {
+            std::cerr << "Number is out of range. Use another number"
+                      << std::endl;
+        }
+        std::cout << avg.get_average() << std::endl;
+    } while (number != 0);
 }
 
 RunningAverage::RunningAverage() { size = 5; }
 RunningAverage::RunningAverage(const int &s) { size = s; }
 
 void RunningAverage::add_value(int value) {
-  d.push_back(value);
-  if (d.size() > size) {
-    d.pop_front();
-  }
+    if (d.size() >= size) {
+        d.pop_front();
+    }
+    d.push_back(value);
 }
 
 double RunningAverage::get_average() {
-  if (d.empty()) return 0;
-  double sum = 0;
-  for (auto n : d) {
-    sum += n;
-  }
-  return sum / (double) d.size(); // In case d.size() < size, d.size() has to be used here
+    if (d.empty())
+        return 0;
+    double sum = 0;
+    for (auto n : d) {
+        sum += n;
+    }
+    return sum /
+           (double)d
+               .size(); // In case d.size() < size, d.size() has to be used here
 }
--- lab4/operator_overloading.cpp
+++ lab4/operator_overloading.cpp
@@ -1,40 +1,25 @@
-#include <deque>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 class Item {
-public:
-  Item(const char *name="", const int weight=0, double price=0, int count=0);
-  int getCount(const uint count);
-  int setCount();
-  double getPrice();
-  int setPrice();
+  public:
+    Item(const char *name0 = "", const int weight0 = 0, const double price0 = 0,
+         const int count0 = 0)
+        : name(name0), weight(weight0), price(price0), count(count0) {}
+    int getCount() { return count; };
+    void setCount(const uint &count0) { count = count0; };
+    double getPrice() { return price; };
+    int setPrice();
+    friend ostream &operator<<(ostream &out, const Item &i);
+    friend istream &operator>>(istream &in, Item &i);
 
-private:
-  string name;
-  int weight;
-  double price;
-  int count;
+  private:
+    string name;
+    int weight;
+    double price;
+    int count;
 };
 
-Item::Item(const char* name0, const int weight0, const double price0, const int count0) : 
-  name(name0), 
-  weight(weight0),
-  price(price0),
-  count(count0)
-{}
-
-int Item::getCount(const uint &index) {
-  return count;
-}
-
-int Item::setCount(const uint &count0) {
-}
-
-
-int main() {
-  
-}
-
+int main() {}
