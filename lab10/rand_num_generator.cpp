6cf3b4f4eea9824f72554bc4e22881c6a000ad7c
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2025-03-06 21:54:40 +0200

    question for teacher added

--- lab10/rand_num_generator.cpp
+++ lab10/rand_num_generator.cpp
@@ -36,18 +36,6 @@ void test_generator(UniqueRng &ur, int count) {
     ur.clear_nums();
 };
 
-int main(void) {
-    UniqueRng randGen(5, 13);
-    test_generator(randGen, 6);
-    test_generator(randGen, 9);
-    test_generator(randGen, 13); // this will cause exception
-    UniqueRng randGen2(1, 35);
-    test_generator(randGen2, 7);
-    test_generator(randGen2, 7);
-    test_generator(randGen2, 7);
-    test_generator(randGen2, 70); // this will cause exception
-}
-
 void UniqueRng::operator()(int count) {
     int number;
     // generate count number of random numbers
@@ -67,4 +55,24 @@ void UniqueRng::operator()(int count) {
         } while (find(numbers.begin(), numbers.end(), number) != numbers.end());
         numbers.push_back(number);
     }
+    /* What if randomizing index of a vector (or array) of "free" numbers?
+    * - finding free numbers is easy using sets, but:
+    *       sets use sorting? (they are atleast sorted)
+    *       set_difference() copies values
+    * 
+    * Would it ever be faster to do this kind of thing instead of spamming rand()
+    * blindly until it hits?
+    */
+}
+
+int main(void) {
+    UniqueRng randGen(5, 13);
+    test_generator(randGen, 6);
+    test_generator(randGen, 9);
+    test_generator(randGen, 13); // this will cause exception
+    UniqueRng randGen2(1, 35);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 70); // this will cause exception
 }
