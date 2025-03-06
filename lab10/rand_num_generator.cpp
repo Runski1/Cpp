371f9e3b6cb1d5b655982bfc04f19b00800af2b3
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2025-03-06 21:33:09 +0200

    lab10 done

---
+++ lab10/rand_num_generator.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+class UniqueRng {
+  public:
+    UniqueRng(int min, int max) : lower(min), upper(max), numbers() {
+        srand(time(NULL)); // seeding RNG rand()
+    }
+    void operator()(int count);
+    const std::vector<int> &get_numbers() const { return numbers; };
+    void clear_nums() { numbers.clear(); };
+
+  private:
+    int lower;
+    int upper;
+    std::vector<int> numbers;
+};
+
+void test_generator(UniqueRng &ur, int count) {
+    std::cout << "Generating numbers:" << std::endl;
+    try {
+        ur(count);
+    } catch (std::runtime_error &err) {
+        std::cout << "Exception: " << err.what() << std::endl;
+    }
+    for (auto a : ur.get_numbers()) {
+        std::cout << a << std::endl;
+    }
+    std::cout << "End of generator" << std::endl;
+    ur.clear_nums();
+};
+
+int main(void) {
+    UniqueRng randGen(5, 13);
+    test_generator(randGen, 6);
+    test_generator(randGen, 9);
+    test_generator(randGen, 13); // this will cause exception
+    UniqueRng randGen2(1, 35);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 7);
+    test_generator(randGen2, 70); // this will cause exception
+}
+
+void UniqueRng::operator()(int count) {
+    int number;
+    // generate count number of random numbers
+    for (int i = 0; i < count; i++) {
+        // generate numbers while vector not full and until unique number is
+        // generated
+        if (numbers.size() >= (upper - lower + 1)) {
+            const std::string err_msg =
+                "Unable to produce unique random number. Tried to generate " +
+                std::to_string(count) + " random numbers. Got only " +
+                std::to_string(i);
+            throw(std::runtime_error(err_msg));
+        }
+        do {
+            // cppreference used this kind to avoid bias
+            number = lower + rand() % (upper - lower + 1);
+        } while (find(numbers.begin(), numbers.end(), number) != numbers.end());
+        numbers.push_back(number);
+    }
+}
